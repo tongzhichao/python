cc441806a049fb41115b5898fdef3357ed08ff70
tongzhichao <710567233@qq.com>
2016-08-31 09:25:31 +0800

    uuid

--- 001.py
+++ 001.py
@@ -2,12 +2,12 @@ import uuid
 
 def create_code(number=200):
 	result = []
-	while True is True:
+	while True is  True:
 		temp = str(uuid.uuid1()).replace('-','')
 		if not temp in result:
 			result.append(temp)
 		if len(result) is number:
 			break
 	return result
-s = create_code()
+s = create_code(10)
 print(s)
