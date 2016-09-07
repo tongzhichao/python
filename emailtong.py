f2b8db82972de11d9b6f1adff696eb071ae24b17
tongzhichao <710567233@qq.com>
2016-09-07 22:51:20 +0800

    email.py_v2.0

--- emailtong.py
+++ emailtong.py
@@ -1,3 +1,45 @@
+#------------------------------v2.0------------------------
+from email import encoders
+from email.header import Header
+from email.mime.text import MIMEText
+from email.utils import parseaddr,formataddr
+
+import smtplib
+def _format_addr(s):
+	name,addr = parseaddr(s)
+	print(name)
+	print(addr)
+	return formataddr((Header(name,'utf-8').encode(),addr))
+
+from_addr = input('From.. ')
+password = input('Password.. ')
+to_addr = input('To.. ')
+smtp_server = input('Smtp server.. ')
+
+msg = MIMEText('hello,send by python..','plain','utf-8')
+
+msg['From'] = _format_addr('Python爱好者<%s>' % from_addr)
+msg['To'] = _format_addr('管理员<%s>' % to_addr)
+msg['subject'] = Header('来着SMTP的问候...','utf-8').encode()
+
+server = smtplib.SMTP_SSL(smtp_server,465)
+#打印出和SMTP服务器交互的所有信息
+server.set_debuglevel(1)
+#登入邮件
+server.login(from_addr,password)
+#发送邮件，内容为msg转成的msg
+server.sendmail(from_addr,[to_addr],msg.as_string())
+server.quit()
+
+#--------------------------------v2.0---------------------
+
+
+
+
+
+
+'''
+#-------------------v1.0-------------------------
 #from email.mime.text import MIMEText
 #注意：不能将文件名叫做email.py，否则会报 ImportError: No module named mime.text
 
@@ -23,3 +65,5 @@ server.login(from_addr,password)
 #发送邮件，内容为msg转成的msg
 server.sendmail(from_addr,[to_addr],msg.as_string())
 server.quit()
+#----------------------------v1.0----------------------
+'''
